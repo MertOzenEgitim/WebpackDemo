4208dba1631b940b040b10bd215f7a1f5b1be1fe
Mert Özen <Mert zen>
2025-02-16 15:22:59 +0300

    5.ders notlari

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,21 +1,23 @@
 const path=require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin}=require('clean-webpack-plugin');
+const MiniCssExtractPlugin=require('mini-css-extract-plugin');
+const webpack  = require('webpack');
 
 module.exports={
     entry:'./src/js/main.js',
     output:{
-        filename:'bundle.js',
-        path:path.resolve(__dirname,'dist'),
-        clean:true
+        filename:'bundle.[hash:7].js',
+        path:path.resolve(__dirname,'dist')
     },
     module:{
         rules:[
             {
                 test:/\.(sa|sc|c)ss$/,
                 use:[
-                'style-loader',
-                'css-loader',
-                'sass-loader' 
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader' 
                 ]
             },
             {
@@ -57,7 +59,13 @@ module.exports={
     },
     plugins:[
         new HtmlWebpackPlugin({
-            template:'./src/index.html'
+            template:'./src/index.html',
+            filename:'index.html'
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({filename:'[name].[hash:7].css'}),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV':JSON.stringify('development')
         })
     ],
     devtool:'source-map',
